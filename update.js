583a9066d27bbdf2b890c48934d1804dbbde039e
andrey5608 <andrey5608@gmail.com>
2021-02-28 00:04:27 +0300

    Update update.js

--- update.js
+++ update.js
@@ -14,6 +14,7 @@ function extract(filepath) {
 
   // HACK: CSVs have different date formats
   const normalDates = dates.map(date => {
+    console.log(date);
     const [month, day] = date.split("/");
     return `2020-${month}-${day}`;
   });
